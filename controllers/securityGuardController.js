680f0045b4c1e3e4ac6743e57f352579e96906c3
CoderRaushan <raushankumar23082004@gmail.com>
2025-11-12 21:15:01 +0530

    guard rooms component and attendence is implemented

--- controllers/securityGuardController.js
+++ controllers/securityGuardController.js
@@ -1,31 +1,6 @@
 
 const Guard = require("../models/SecurityGuard.js");
 
-// Create
-// exports.addGuard = async (req, res) => {
-//   try {
-//     const { name, guardId, shift, post, contact, status } = req.body;
-//     if (!name || !guardId || !shift || !post || !contact || !status) {
-//       return res.status(400).json({ success: false, message: "All fields required" });
-//     }
-//     console.log(req.body)
-//     const existing = await Guard.findOne({ guardId });
-//     console.log("existing",existing);
-//     if (existing) {
-//       return res.status(400).json({ success: false, message: "Guard ID already exists" });
-//     }
-//     console.log("before save")
-//     const guard = new Guard({ name, guardId, shift, post, contact, status });
-//     await guard.save();
-//     console.log("after saved",guard);
-//     res.json({ success: true, guard });
-//   } catch (err) {
-//     res.status(500).json({ success: false, message: err.message });
-//   }
-// };
-
-// Read all
-
 exports.getAllGuards = async (req, res) => {
   try {
     const { hostelNo } = req.query;
@@ -61,12 +36,10 @@ exports.updateGuard = async (req, res) => {
   }
 };
 
-exports.getGuardById=async(req,res)=>
-{
-   const { id } = req.body;
+exports.getGuardById = async (req, res) => {
+  const { id } = req.body;
   try {
     const guard = await Guard.findById(id).select("-password");
-    console.log(guard);
     if (!guard) {
       return res.status(404).json({ success: false, message: "Guard not found" });
     }
