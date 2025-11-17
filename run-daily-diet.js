a4ce2f6977f4c677d23b47f5289c1fe56286696a
CoderRaushan <raushankumar23082004@gmail.com>
2025-11-18 02:11:49 +0530

    github action bug is fixed

--- run-daily-diet.js
+++ run-daily-diet.js
@@ -1,7 +1,14 @@
-import runDailyDietJob from "./utils/dailyDietJob.js";
+// run-daily-diet.js (CommonJS)
+const runDailyDietJob = require('./utils/dailyDietJob.js');
 
 (async () => {
   console.log("Starting Daily Diet Job:", new Date());
-  await runDailyDietJob();
-  console.log("Finished Daily Diet Job:", new Date());
+  try {
+    await runDailyDietJob();
+    console.log("Finished Daily Diet Job:", new Date());
+    process.exit(0);
+  } catch (err) {
+    console.error('runDailyDietJob failed:', err);
+    process.exit(1);
+  }
 })();
